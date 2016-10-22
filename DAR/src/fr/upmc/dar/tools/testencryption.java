077ee7d12c827f168763cc5619d47e41e1552d86
alphabridgeUPMC <mokrane.kadri@etu.upmc.fr>
2016-10-23 01:17:32 +0200

    !

--- DAR/src/fr/upmc/dar/tools/testencryption.java
+++ DAR/src/fr/upmc/dar/tools/testencryption.java
@@ -5,7 +5,7 @@ public class testencryption {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 PasswordEncryptor enc = new PasswordEncryptor();
-		String encrypted_pass =enc.encryptPassword("kokakola&2010");
+		String encrypted_pass =enc.encryptPassword("king");
 		boolean res = enc.checkPassword("kk", encrypted_pass);
 		System.out.println(res);
 		System.out.println(encrypted_pass.length());
