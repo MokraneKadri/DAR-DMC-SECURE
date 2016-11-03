51b1780b5f189a71fae01d51834aee7ac593189e
Gumiko <cedric.ribeiro@etu.upmc.fr>
2016-11-03 18:26:25 +0100

    change url to relative

--- DAR/src/fr/upmc/dar/filters/ConnectedModeFilter.java
+++ DAR/src/fr/upmc/dar/filters/ConnectedModeFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -68,4 +69,16 @@ public class ConnectedModeFilter implements Filter {
 	
 
 }
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
 }
