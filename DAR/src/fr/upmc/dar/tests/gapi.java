d6246613efe993468314b9c2a3da2c7618dc6d35
alphabridgeUPMC <mokrane.kadri@etu.upmc.fr>
2016-11-10 12:53:57 +0100

    new eventlisting style

--- DAR/src/fr/upmc/dar/tests/gapi.java
+++ DAR/src/fr/upmc/dar/tests/gapi.java
@@ -8,6 +8,7 @@ import com.google.maps.DirectionsApiRequest;
 import com.google.maps.DistanceMatrixApi;
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
+import com.google.maps.GeocodingApiRequest;
 import com.google.maps.model.DirectionsRoute;
 import com.google.maps.model.DistanceMatrix;
 import com.google.maps.model.GeocodingResult;
@@ -20,17 +21,21 @@ public class gapi {
 
 		GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyBaecIdNb-rbp-gzGV9q9f_v1gAeEjgF4c");
 		GeocodingResult[] results;
+		
+		
+		
 		try {
 			results = GeocodingApi.geocode(context,
-			    "1600 Amphitheatre Parkway Mountain View, CA 94043").await();
+			    "3 rue du général lambert 75007 paris").await();
 		
-			System.out.println(results[0].formattedAddress);
+	
 			 DateTime now = new DateTime();
 			 DirectionsRoute[] result = DirectionsApi.getDirections(context, "3,rue du general lambert 75007 paris",
 				        "100 rue gabriel péri 93200").departureTime(now).mode(TravelMode.WALKING).await();
 			 System.out.println(result[0].legs[0].startAddress);
 			 System.out.println(result[0].legs[0].endAddress);
 			 System.out.println(result[0].legs[0].duration);
+			 System.out.println();
 			// System.out.println(result[0].summary);
 			 
 			 String[] origin = new String[]{"100 rue gabriel péri 93200"};
