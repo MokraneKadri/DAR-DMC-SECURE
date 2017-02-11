e05f40394350c4bde924005e18a763b48a99d235
alphabridgeUPMC <mokrane.kadri@etu.upmc.fr>
2017-02-11 11:31:41 +0100

    DAR2 initial

--- DAR/src/fr/upmc/dar/dao/CommentDao.java
+++
@@ -1,7 +0,0 @@
-package fr.upmc.dar.dao;
-
-import fr.upmc.dar.dao.interfaces.IComment;
-
-public class CommentDao  implements IComment{
-
-}
--- DAR/src/fr/upmc/dar/enums/LoginType.java
+++
@@ -1,7 +0,0 @@
-package fr.upmc.dar.enums;
-
-public enum LoginType {
-
-	EMAIL,
-	USERNAME
-}
